1a62a5da19caedadc61bd24ac937f9eda502236d
mrsaninditaroy <53885179+mrsaninditaroy@users.noreply.github.com>
2023-04-17 10:06:29 +0530

    Initial commit

---
+++ ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins.demo.jenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
